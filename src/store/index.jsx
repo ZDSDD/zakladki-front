95246446500588d6cf5700bf779fe7290e3ceb4a
zdsdd <sebasadowy@gmail.com>
2024-10-03 13:39:30 +0200

    prep for making api requests

---
+++ src/store/index.jsx
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+
+export const store = configureStore({
+    reducer: {
+
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware();
+    }
+})
+
+setupListeners(store.dispatch);
