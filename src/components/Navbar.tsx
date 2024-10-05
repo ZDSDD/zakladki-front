859c6a62a77434988d0d26b157c330906b196e6e
zdsdd <sebasadowy@gmail.com>
2024-10-05 21:04:11 +0200

    make navbar content centered

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,24 @@
 import { Link } from "react-router-dom"; // Optional: Use if you're using React Router for navigation
-
+import "./navbar.css";
 const Navbar = () => {
   return (
-    <nav className="shadow-md sticky top-0 z-50 w-full border-border/40 bg-background/95 backdrop:blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
-        <div className="text-slate-900 text-2xl font-bold pl-5 pr-4">
-          My App
+    <nav className="shadow-md sticky top-0 z-50 w-full border-b border-green-500 bg-background/30 backdrop:blur supports-[backdrop-filter]:bg-background/80">
+      <div className="container grid h-12 max-w-screen-2xl items-center grid-cols-[1fr_auto_1fr]">
+        {/* Left side links */}
+        <div className="flex justify-end mr-5 hover:text-green-600 transition-colors duration-500">
+          <Link to="/bookmarks">Bookmarks</Link>
         </div>
-        <div className="space-x-4">
-          <Link to="/" className="hover:text-blue-200">
-            Home
+
+        {/* Centered title */}
+        <div className="text-slate-900 text-5xl font-bold text-center pl-2 pr-2">
+          <Link className="glow" to={"/"}>
+            Dziennik Nudziary
           </Link>
-          <Link to="/bookmarks" className="hover:text-blue-200">
-            Bookmarks
-          </Link>
-          <Link to="/about" className="hover:text-blue-200">
-            About
-          </Link>
+        </div>
+
+        {/* Right side links */}
+        <div className="flex justify-start ml-5 hover:text-green-600 transition-colors duration-500">
+          <Link to="/about">About</Link>
         </div>
       </div>
     </nav>
