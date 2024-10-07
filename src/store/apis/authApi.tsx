2f7adde79c80ce6ef6960e1f7a56c58c3fa03029
zdsdd <sebasadowy@gmail.com>
2024-10-07 23:28:19 +0200

    fix eslint/no-explicit-any in authApi.tsx

--- src/store/apis/authApi.tsx
+++ src/store/apis/authApi.tsx
@@ -1,65 +1,70 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query';
 import { RootState } from "@/store";
 import { LoginResponse, LoginCredentials, User } from "@/types/auth";
 import { setCredentials, logOut } from "@/reducers/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:8080/api/users",
-  credentials: "include",
-  prepareHeaders: (headers, { getState }) => {
-    const token = (getState() as RootState).auth.token;
-    if (token) {
-      console.log("Token is present, set header with bearer");
+    baseUrl: "http://localhost:8080/api/users",
+    credentials: "include",
+    prepareHeaders: (headers, { getState }) => {
+        const token = (getState() as RootState).auth.token;
+        if (token) {
+            console.log("Token is present, set header with bearer");
 
-      headers.set("authorization", `Bearer ${token}`);
-    } else {
-      console.log("NO TOKEN FOUND :(");
-    }
-    return headers;
-  },
+            headers.set("authorization", `Bearer ${token}`);
+        } else {
+            console.log("NO TOKEN FOUND :(");
+        }
+        return headers;
+    },
 });
 
-const baseQueryWithReauth = async (args: any, api: any, extraOptions: any) => {
-  let result = await baseQuery(args, api, extraOptions);
+const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
+    args,
+    api,
+    extraOptions
+) => {
+    let result = await baseQuery(args, api, extraOptions);
 
-  if (result?.error?.status === 401) {
-    // Try to get a new token
-    console.log("trying to get new token");
-    const refreshResult = await baseQuery(
-      { url: "refresh", method: "POST" },
-      api,
-      extraOptions,
-    );
-    if (refreshResult?.data) {
-      const user = (api.getState() as RootState).auth.user as User;
-      // Store the new token
-      api.dispatch(
-        setCredentials({ user, token: refreshResult.data as string }),
-      );
-      // Retry the original query with new token
-      result = await baseQuery(args, api, extraOptions);
-    } else {
-      api.dispatch(logOut());
+    if (result?.error?.status === 401) {
+        // Try to get a new token
+        console.log("trying to get new token");
+        const refreshResult = await baseQuery(
+            { url: "refresh", method: "POST" },
+            api,
+            extraOptions,
+        );
+        if (refreshResult?.data) {
+            const user = (api.getState() as RootState).auth.user as User;
+            // Store the new token
+            api.dispatch(
+                setCredentials({ user, token: refreshResult.data as string }),
+            );
+            // Retry the original query with new token
+            result = await baseQuery(args, api, extraOptions);
+        } else {
+            api.dispatch(logOut());
+        }
     }
-  }
 
-  return result;
+    return result;
 };
 
 export const authApi = createApi({
-  baseQuery: baseQueryWithReauth,
-  endpoints: (builder) => ({
-    login: builder.mutation<LoginResponse, LoginCredentials>({
-      query: (credentials) => ({
-        url: "/login",
-        method: "POST",
-        body: credentials,
-      }),
+    baseQuery: baseQueryWithReauth,
+    endpoints: (builder) => ({
+        login: builder.mutation<LoginResponse, LoginCredentials>({
+            query: (credentials) => ({
+                url: "/login",
+                method: "POST",
+                body: credentials,
+            }),
+        }),
+        protected: builder.query<{ message: string }, void>({
+            query: () => "protected",
+        }),
     }),
-    protected: builder.query<{ message: string }, void>({
-      query: () => "protected",
-    }),
-  }),
 });
 
 export const { useLoginMutation, useProtectedQuery } = authApi;
