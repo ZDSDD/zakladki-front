004556c2e867a96d790af18aff4120fb551b9bda
zdsdd <sebasadowy@gmail.com>
2024-10-08 18:38:08 +0200

    better style on login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,41 +2,44 @@ import Login from "@/components/Login";
 import Register from "@/components/Register";
 import { useState } from "react";
 function LoginPage() {
-  const [notRegisteredYet, setNotRegisteredYet] = useState<boolean>(false);
+    const [notRegisteredYet, setNotRegisteredYet] = useState<boolean>(false);
 
-  const formStyles: string =
-    "border border-slate-200 shadow-[5px_5px_30px_-15px_rgba(0,0,0,0.3)]";
+    const formStyles: string =
+        "border border-slate-200 shadow-[5px_5px_30px_-15px_rgba(0,0,0,0.3)]";
 
-  const getForm = () => {
-    if (notRegisteredYet) {
-      return (
-        <>
-          <Register className={formStyles}></Register>
-        </>
-      );
-    }
+    const getForm = () => {
+        if (notRegisteredYet) {
+            return (
+                <>
+                    <Register className={formStyles}></Register>
+                </>
+            );
+        }
+        return (
+            <>
+                <Login className={formStyles} />
+            </>
+        );
+    };
+
     return (
-      <>
-        <Login className={formStyles} />
-      </>
-    );
-  };
+        <div className="flex justify-center items-center flex-col min-h-screen bg-gray-100">
+            <div className="border-1 m-1 p-6 rounded-lg shadow-lg bg-white min-w-96">
+                {getForm()}
+            </div>
+            <div
+                className="mt-4 text-blue-600 hover:cursor-pointer hover:underline transition-colors duration-300"
+                onClick={() => setNotRegisteredYet((oldVal) => !oldVal)}
+            >
+                {notRegisteredYet ? (
+                    <span>Masz już konto? Zaloguj się</span>
+                ) : (
+                    <span>Nie masz konta? Zarejestruj się</span>
+                )}
+            </div>
+        </div>
 
-  return (
-    <div className="flex justify-center items-center flex-col m-auto transition-all duration-1000 delay-500">
-      {getForm()}
-      <div
-        className="hover:cursor-pointer hover:underline"
-        onClick={() => setNotRegisteredYet((oldVal) => !oldVal)}
-      >
-        {notRegisteredYet ? (
-          <span>Masz już konto? Zaloguj się</span>
-        ) : (
-          <span>Nie masz konta? Zarejestruj się</span>
-        )}
-      </div>
-    </div>
-  );
+    );
 }
 
 export default LoginPage;
