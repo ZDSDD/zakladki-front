ec7cde674f7d79bb14fdf2f33fa5216b5b47108e
zdsdd <sebasadowy@gmail.com>
2025-01-08 19:35:55 +0100

    add menu on user click

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,18 @@
+import React from 'react';
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { RootState } from "@/store";  // Import the RootState type from your store
+import { RootState } from "@/store";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Settings, User, LogOut } from "lucide-react";
 
 const Navbar = () => {
-    const user = useSelector((state: RootState) => state.auth.user);  // Access user from authSlice
+    const user = useSelector((state: RootState) => state.auth.user);
 
     return (
         <nav className="shadow-md sticky top-0 z-50 w-full border-b border-green-500 bg-background/30 backdrop:blur supports-[backdrop-filter]:bg-background/80">
@@ -21,10 +30,29 @@ const Navbar = () => {
                 <div className="flex justify-center items-center">
                     {/* Optional Center Content */}
                 </div>
-                {/* Right side login/welcome */}
+                {/* Right side login/welcome with dropdown */}
                 <div className="flex justify-end items-center">
                     {user ? (
-                        <span className="hover:text-green-600 transition-colors duration-500">Hello, {user.name}</span>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger className="hover:text-green-600 transition-colors duration-500 outline-none">
+                                <span>Hello, {user.name}</span>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="w-48">
+                                <DropdownMenuItem className="cursor-pointer">
+                                    <User className="mr-2 h-4 w-4" />
+                                    <Link to="/profile">Profile</Link>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem className="cursor-pointer">
+                                    <Settings className="mr-2 h-4 w-4" />
+                                    <Link to="/settings">Settings</Link>
+                                </DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem className="cursor-pointer text-red-600">
+                                    <LogOut className="mr-2 h-4 w-4" />
+                                    <span>Logout</span>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
                     ) : (
                         <Link to="/login" className="hover:text-green-600 transition-colors duration-500 ml-5">
                             Login
